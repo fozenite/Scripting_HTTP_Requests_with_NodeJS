381474a9859afe910c8a12223ba1480d47c796bb
Rohit Moza <moza.rohit@gmail.com>
2017-04-10 16:50:08 +0000

    Printed in chunks

--- Step1-Printing_in_chunks.js
+++ Step1-Printing_in_chunks.js
@@ -1,3 +1,10 @@
+
+// IMPORT HTTP MODULE
+var https = require('https');
+
+
+
+
 function getAndPrintHTMLChunks () {
 
   var requestOptions = {
@@ -6,5 +13,32 @@ function getAndPrintHTMLChunks () {
   };
 
   /* Add your code here */
+  https.get(requestOptions, function(response) {
+
+    //set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+
+    // Call back invoked when a 'data' chunk is received
+    response.on('data', function (data) {
+      console.log('Chunk received' + data + '\n');
+    });
+
+    //Callback invoked when all the data has been received and it is end of stream
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+
+    });
+
+
+  });
 
 }
+
+getAndPrintHTMLChunks();
+
+
+
+
+
