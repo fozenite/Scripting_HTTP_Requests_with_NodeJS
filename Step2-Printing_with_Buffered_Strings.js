963ade6b72449c6c6b3141b588f48a18c3cf9088
Rohit Moza <moza.rohit@gmail.com>
2017-04-10 16:53:56 +0000

    My first commit

---
+++ Step2-Printing_with_Buffered_Strings.js
@@ -0,0 +1,39 @@
+
+// IMPORT HTTP MODULE
+var https = require('https');
+
+
+
+
+function getAndPrintHTML() {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  /* Add your code here */
+  https.get(requestOptions, function(response) {
+
+    //set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+
+    // Call back invoked when a 'data' chunk is received
+    response.on('data', function (data) {
+      console.log('Chunk received' + data + '\n');
+    });
+
+    //Callback invoked when all the data has been received and it is end of stream
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+
+    });
+
+
+  });
+
+}
+
+getAndPrintHTML();
